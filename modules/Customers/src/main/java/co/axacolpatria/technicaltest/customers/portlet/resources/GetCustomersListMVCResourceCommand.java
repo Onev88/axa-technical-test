619e3ea34987ac12d43990e586b3cb3ee4a0fd25
Juan Victor Martinez <desarrollador3.siivra@gmail.com>
2022-09-06 21:53:05 -0500

    Se renombra clase por error en mayuscula inicial

--- modules/Customers/src/main/java/co/axacolpatria/technicaltest/customers/portlet/resources/GetCustomersListMVCResourceCommand.java
+++ modules/Customers/src/main/java/co/axacolpatria/technicaltest/customers/portlet/resources/GetCustomersListMVCResourceCommand.java
@@ -20,12 +20,12 @@ import co.axacolpatria.technicaltest.customers.constants.CustomersPortletKeys;
 @Component(immediate = true, property = {
 		"javax.portlet.name=" + CustomersPortletKeys.CUSTOMERS, 
 		"mvc.command.name=" + CustomersPortletKeys.RESOURCE_GET_CUSTOMERS_LIST}, service = MVCResourceCommand.class)
-public class GetCustomersListMVCResourceCommand implements MVCResourceCommand{
+public class getCustomersListMVCResourceCommand implements MVCResourceCommand{
 	
 	@Reference
 	private CustomersClient customerClientUtil;
 	
-	private static final Log LOG = LogFactoryUtil.getLog(GetCustomersListMVCResourceCommand.class);
+	private static final Log LOG = LogFactoryUtil.getLog(getCustomersListMVCResourceCommand.class);
 
 	@Override
 	public boolean serveResource(ResourceRequest request, ResourceResponse response)
